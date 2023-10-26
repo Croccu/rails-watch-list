aad19f826f0f059c3d09e498b50c55921adcbd9c
rico paum <ricsens@me.com>
2023-10-26 14:31:01 +0400

    List controllers and views created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+api_url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+json_data = JSON.parse(URI.open(api_url).read)
+
+json_data['results'].each do |movie_data|
+  movie = Movie.create(
+    title: movie_data['title'],
+    overview: movie_data['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie_data['poster_path']}",
+    rating: movie_data['vote_average']
+  )
+end
